30bcb00de2fd8c0cb6e5340dd052b787992995ae
Sindre Bøyum <sindre.boyum@gmail.com>
2020-10-20 11:24:20 +0200

    Store language choice in localStorage

--- stores/LanguageStore.tsx
+++ stores/LanguageStore.tsx
@@ -1,11 +1,23 @@
-import { useReducer } from 'react';
+import { useEffect, useReducer } from 'react';
 import LanguageContext from '../contexts/LanguageContext';
-import { languageReducer } from '../reducers/language.reducer';
 import LanguageEnum from '../enums/Language';
+import { languageReducer } from '../reducers/language.reducer';
 
 export default function LanguageStore({ children }: { children: JSX.Element | JSX.Element[] }): JSX.Element {
-  const [language, dispatch] = useReducer(languageReducer, LanguageEnum.NorskBokmal);
+  const localStorageKey = 'language';
 
+  const defaultLanguage = LanguageEnum.NorskBokmal;
+  let cachedLanguage: LanguageEnum;
+  
+  const isBrowser = process.browser;
+  if (isBrowser) {
+    cachedLanguage = LanguageEnum[localStorage.getItem(localStorageKey)];
+    
+    useEffect(() => localStorage.setItem(localStorageKey, language));
+  }
+  
+  const [language, dispatch] = useReducer(languageReducer, cachedLanguage ?? defaultLanguage);
+  
   return (
     <LanguageContext.Provider value={[language, dispatch]}>
       {children}
