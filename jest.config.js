57e4dc8de57903171442b628cf06bd9b13b7174a
Sindre Bøyum <sindre.boyum@gmail.com>
2020-10-25 18:51:37 +0100

    Use identity-obj-proxy to bypass css modules

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   moduleNameMapper: {
-    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
-      "<rootDir>/__mocks__/fileMock.js",
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
+    '\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
   },
 };
