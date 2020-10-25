e87a504b34bbef55dbbb692727515f79c8ee5c4b
Restyled.io <commits@restyled.io>
2020-10-25 18:28:18 +0000

    Restyled by clang-format

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -14,11 +14,13 @@
 //
 //
 // -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
+// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options)
+// => { ... })
 //
 //
 // -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
+// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject,
+// options) => { ... })
 //
 //
 // -- This will overwrite an existing command --
--- cypress/support/index.js
+++ cypress/support/index.js
@@ -16,5 +16,5 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 
-// Alternatively you can use CommonJS syntax:
-// require('./commands')
+    // Alternatively you can use CommonJS syntax:
+    // require('./commands')
