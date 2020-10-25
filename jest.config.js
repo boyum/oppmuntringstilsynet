e808b24716e221b19a0677c9431987452357af42
Restyled.io <commits@restyled.io>
2020-10-25 19:45:04 +0000

    Restyled by prettier

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  moduleNameMapper : {
-    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$' :
-        '<rootDir>/__mocks__/fileMock.js',
-    '\\.module\\.(css|sass|scss)$' : 'identity-obj-proxy',
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/__mocks__/fileMock.js",
+    "\\.module\\.(css|sass|scss)$": "identity-obj-proxy",
   },
 };
