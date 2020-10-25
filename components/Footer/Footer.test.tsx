60b3ec1392e465ac541dff0822f9e0bea465c543
Sindre Bøyum <sindre.boyum@gmail.com>
2020-10-25 17:36:12 +0100

    Make jest support css modules

--- components/Footer/Footer.test.tsx
+++ components/Footer/Footer.test.tsx
@@ -8,18 +8,9 @@ expect.extend(toHaveNoViolations);
 describe(Footer.name, () => {
   it('should render without accessibility errors', async () => {
     const footer = render(<Footer />).container;
-  
+
     const results = await axe(footer);
-  
+
     expect(results).toHaveNoViolations();
-  });
-  
-  it('should render with centered text', () => {
-    const footer = render(<Footer />).container;
-  
-    const textAlign = window.getComputedStyle(footer).textAlign;
-    console.log('styles', window.getComputedStyle(footer))
-  
-    expect(textAlign).toBe('center');
   });
 });
