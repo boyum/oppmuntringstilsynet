6335290afeed17e5468a3ba33dd26f24ab957400
Sindre Bøyum <boyum@users.noreply.github.com>
2020-12-25 09:44:15 +0100

    Fix snow

--- public/snow.js
+++ public/snow.js
@@ -61,7 +61,7 @@
 
     canvas.style.width = '100%';
     canvas.style.height = '100%';
-    canvas.style.position = 'absolute';
+    canvas.style.position = 'fixed';
     canvas.style.left = '0';
     canvas.style.top = '0';
     canvas.style.pointerEvents = 'none';
