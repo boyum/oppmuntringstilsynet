5ce2d554d7b1917b5a2abe68fc247cca99e62dd6
Sindre Bøyum <sindre.boyum@gmail.com>
2021-06-08 10:35:25 +0200

    Add hover/focus and click effects

--- components/Buttons/index.tsx
+++ components/Buttons/index.tsx
@@ -1,7 +1,7 @@
 import React, { MouseEventHandler, useContext } from "react";
 import LanguageContext from "../../contexts/LanguageContext";
 import { getTranslations } from "../../pages/api/translations";
-import styles from "./Buttons.module.css";
+import styles from "./Buttons.module.scss";
 
 type Props = {
   handleReset: MouseEventHandler<HTMLButtonElement>;
@@ -18,10 +18,14 @@ export default function Buttons({
   return (
     <div className={styles.buttons}>
       <button type="button" className={styles.copyLink} onClick={handleCopy}>
-        {translations.copyButtonText}
+        <div className={styles.buttonInner}>
+          {translations.copyButtonText}
+        </div>
       </button>
       <button type="button" className={styles.reset} onClick={handleReset}>
-        {translations.resetButtonText}
+        <div className={styles.buttonInner}>
+          {translations.resetButtonText}
+        </div>
       </button>
     </div>
   );
