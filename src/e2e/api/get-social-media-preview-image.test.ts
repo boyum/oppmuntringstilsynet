f5c13b22ab224762a70920283427b27760f0af0c
Sindre Bøyum <sindre.boyum@gmail.com>
2021-07-07 11:18:44 +0200

    add e2e tests for preview image

---
+++ src/e2e/api/get-social-media-preview-image.test.ts
@@ -0,0 +1,47 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import dotenv from "dotenv";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import getSocialMediaPreviewImage from "../../../api/get-social-media-preview-image";
+
+dotenv.config();
+
+const localUrl = "http://localhost:3000";
+const deployUrl = process.env.DEPLOY_URL ?? localUrl;
+const endpointUrl = `${deployUrl}/api/get-social-media-preview-image`;
+
+describe(getSocialMediaPreviewImage.name, () => {
+  it("should return an image when no encoded message is provided", async () => {
+    const isLocal = localUrl === deployUrl;
+
+    const result = await page.goto(endpointUrl);
+
+    const pageDoesNotExist = result.status() === 404;
+    if (pageDoesNotExist && isLocal) {
+      throw new Error(
+        `Page '${endpointUrl}' does not exist.
+        Did you start the vercel dev server? If not, run \`npm run dev:api\` and try again.`,
+      );
+    }
+
+    expect(result.status()).toBe(200);
+    expect(result.headers()["Content-Type"]).toBe("image/png");
+  });
+
+  it("should return an image when an encoded message is provided", async () => {
+    const isLocal = localUrl === deployUrl;
+    const url = `${endpointUrl}/?m=N4IgxgFgpmDWDOIBcBtALgJwK5QDSZ32ygF1cQATAQzSmUprvIFsp54qBzOpEV9rkxAA7Kq3qjx5ADZVhnLIPoA5APYZ4sAEKrYzKtJDk00VsrE8QABwwBLCnQC%2BQA`;
+
+    const result = await page.goto(url);
+
+    const pageDoesNotExist = result.status() === 404;
+    if (pageDoesNotExist && isLocal) {
+      throw new Error(
+        `Page '${endpointUrl}' does not exist.
+        Did you start the vercel dev server? If not, run \`npm run dev:api\` and try again.`,
+      );
+    }
+
+    expect(result.status()).toBe(200);
+    expect(result.headers()["Content-Type"]).toBe("image/png");
+  });
+});
