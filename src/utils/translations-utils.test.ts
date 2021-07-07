7d1ddb5eb600f29753d62fdf52756b4daefd002e
Sindre Bøyum <sindre.boyum@gmail.com>
2021-07-07 15:36:25 +0200

    add translations tests

---
+++ src/utils/translations-utils.test.ts
@@ -0,0 +1,30 @@
+import LanguageEnum from "../enums/Language";
+import languages from "../models/languages";
+import Translations from "../types/Translations";
+import { getLanguage } from "./translations-utils";
+
+export function getTranslations(language: LanguageEnum): Translations {
+  return getLanguage(language).translations;
+}
+
+describe(getLanguage.name, () => {
+  it("should return the language that corresponds to the provided language name", () => {
+    const languageName = LanguageEnum.English;
+
+    const expectedLanguage = languages.English;
+    const actualLanguage = getLanguage(languageName);
+
+    expect(actualLanguage).toEqual(expectedLanguage);
+  });
+});
+
+describe(getTranslations.name, () => {
+  it("should return translations of the language that corresponds to the provided language name", () => {
+    const languageName = LanguageEnum.English;
+
+    const expectedTranslations = languages.English.translations;
+    const actualTranslations = getTranslations(languageName);
+
+    expect(actualTranslations).toEqual(expectedTranslations);
+  });
+});
