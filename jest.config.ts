648be08592258873775280da8e403782fad075b4
boyum <sindre.boyum@tietoevry.com>
2022-02-02 08:42:34 +0100

    remove babel, use next/jest

--- jest.config.ts
+++ jest.config.ts
@@ -1,15 +1,15 @@
+/* eslint-disable import/no-default-export */
 import type { Config } from "@jest/types";
-// eslint-disable-next-line import/no-extraneous-dependencies
 import { defaults } from "jest-config";
+import nextJest from "next/jest";
 
-const config: Config.InitialOptions = {
-  moduleNameMapper: {
-    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
-      "<rootDir>/__mocks__/fileMock.js",
-    "\\.module\\.(css|sass|scss)$": "identity-obj-proxy",
-  },
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+const customConfig: Config.InitialOptions = {
   testPathIgnorePatterns: [...defaults.testPathIgnorePatterns, "e2e"],
   testEnvironment: "jsdom",
 };
 
-export default config;
+export default createJestConfig(customConfig);
