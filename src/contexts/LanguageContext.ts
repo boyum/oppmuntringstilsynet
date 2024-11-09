8ceb479561ac72e06654270b1e4113ec29aa30ed
Sindre Bøyum <sindre.boyum@tietoevry.com>
2024-11-09 15:18:44 +0100

    chore: use null value as default in language context

--- src/contexts/LanguageContext.ts
+++ src/contexts/LanguageContext.ts
@@ -4,9 +4,9 @@ import { defaultLanguage } from "../utils/language-utils";
 
 const defaultState: [LanguageEnum, (language: LanguageEnum) => void] = [
   defaultLanguage,
-  () => {
-    /* Intentionally empty */
-  },
+  // @ts-expect-error this is `null` at first,
+  // then React updates it behind the scenes.
+  null,
 ];
 
 export const LanguageContext = createContext(defaultState);
